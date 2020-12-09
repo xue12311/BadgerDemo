76b8f558d9176e3a9a84c13773a158f74dcdcfe3
xue <1712530035@qq.com>
2020-12-09 10:34:07 +0800

    添加辅助方法

---
+++ app_common_library/src/main/java/com/zjx/app_common_library/utils/ext/ListExt.kt
@@ -0,0 +1,29 @@
+package com.zjx.app_common_library.utils.ext
+
+import com.blankj.utilcode.util.StringUtils
+
+
+fun List<String>.forEachListToString(): String {
+    var mStringBuffer = StringBuffer()
+    this.forEach {
+        if (mStringBuffer.toString().isNoTrimEmpty()) {
+            mStringBuffer.append(",")
+        }
+        mStringBuffer.append(it)
+    }
+    return mStringBuffer.toString()
+}
+
+fun <T> List<T>.forEachListToString(getParameterToString: (T) -> String?): String {
+    var mStringBuffer = StringBuffer()
+    this.forEach {
+        val sParameter = getParameterToString(it)
+        if (sParameter != null && sParameter.isNoTrimEmpty()) {
+            if (mStringBuffer.toString().isNoTrimEmpty()) {
+                mStringBuffer.append(",")
+            }
+            mStringBuffer.append(sParameter)
+        }
+    }
+    return mStringBuffer.toString()
+}
---
+++ app_common_library/src/main/java/com/zjx/app_common_library/utils/ext/StringExt.kt
@@ -0,0 +1,8 @@
+package com.zjx.app_common_library.utils.ext
+
+import com.blankj.utilcode.util.StringUtils
+
+fun String.isNoTrimEmpty(): Boolean = !StringUtils.isTrimEmpty(this)
+fun String.isTrimEmpty(): Boolean = StringUtils.isTrimEmpty(this)
+fun String.isEmpty(): Boolean = StringUtils.isEmpty(this)
+fun String.isNoEmpty(): Boolean = !StringUtils.isEmpty(this)
