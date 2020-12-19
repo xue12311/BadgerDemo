43a24a8f375c627cae7b489bf43ab38fe7cb5eac
xue <1712530035@qq.com>
2020-12-19 10:25:17 +0800

    去掉角标帮助类

--- app/src/main/java/com/huashi/badgerdemo/MainActivity.java
+++ app/src/main/java/com/huashi/badgerdemo/MainActivity.java
@@ -2,14 +2,11 @@ package com.huashi.badgerdemo;
 
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.StringUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.zjx.app_common_library.utils.WeakHandler;
 
 public class MainActivity extends AppCompatActivity {
 
