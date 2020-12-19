1340c45da82d701692bade8731f12beec9bb0fbb
xue <1712530035@qq.com>
2020-12-19 10:21:00 +0800

    去掉角标帮助类

--- app/src/main/java/com/huashi/badgerdemo/MainActivity.java
+++ app/src/main/java/com/huashi/badgerdemo/MainActivity.java
@@ -10,7 +10,6 @@ import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.StringUtils;
 import com.blankj.utilcode.util.ToastUtils;
 import com.zjx.app_common_library.utils.WeakHandler;
-import com.zjx.app_common_library.utils.shortcutbadger.ShortcutBadger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,32 +22,9 @@ public class MainActivity extends AppCompatActivity {
             if (StringUtils.isTrimEmpty(etNum.getText().toString())) {
                 ToastUtils.showShort("请填写角标数量");
             } else {
-                //延迟显示角标个数，解决小米不正确显示角标
-                new WeakHandler().postDelayed(() -> {
-                    int badgeCount = 0;
-                    try {
-                        badgeCount = Integer.parseInt(etNum.getText().toString());
-                    } catch (NumberFormatException e) {
-                        Toast.makeText(getApplicationContext(), "Error input", Toast.LENGTH_SHORT).show();
-                    }
-                    boolean success = ShortcutBadger.applyCount(MainActivity.this, badgeCount);
-                    if (success) {
-                        ActivityUtils.startHomeActivity();
-                        ToastUtils.showShort("角标设置成功，角标数量: " + badgeCount);
-                    } else {
-                        ToastUtils.showShort("角标设置失败");
-                    }
-                }, 500);
             }
         });
         findViewById(R.id.btnRemoveBadge).setOnClickListener(view -> {
-            boolean success = ShortcutBadger.removeCount(MainActivity.this);
-            if (success) {
-                ActivityUtils.startHomeActivity();
-                ToastUtils.showShort("清除角标成功");
-            } else {
-                ToastUtils.showShort("清除角标失败");
-            }
         });
     }
 }
--- app_common_library/src/main/java/com/zjx/app_common_library/utils/shortcutbadger/impl/IntentConstants.java
+++
@@ -1,6 +0,0 @@
-package com.zjx.app_common_library.utils.shortcutbadger.impl;
-
-public interface IntentConstants {
-    String DEFAULT_INTENT_ACTION = "android.intent.action.BADGE_COUNT_UPDATE";
-    String DEFAULT_OREO_INTENT_ACTION = "com.zjx.app_common_library.utils.shortcutbadger.BADGE_COUNT_UPDATE";
-}
